8da38b97f5dfd24062588137ba2bbd9948dda380
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-03-20 17:33:02 -0300

    primeiro commit.

---
+++ Solution01/Projeto01/Projeto01/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto01 {
+    public class MyClass {
+        public static int Soma(int a, int b) {
+            return a + b;
+        }
+    }
+}
